c09ecc20b78769cb9658247d7934d4d2c910c916
Yauheni Dubouski <joinhud@gmail.com>
2018-03-01 18:47:01 +0300

    longest-consecutive-sequence/feature: implement business logic for finding longest consecutive sequence length

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
+  const arrayLength = array.length;
+  const set = new Set(array);
+  let result = 0;
+
+  for (let i = 0; i < arrayLength; i++) {
+    if (!set.has(array[i] - 1)) {
+      let j = array[i];
+
+      while (set.has(j)) {
+        j++;
+
+        if (result < j - array[i]) {
+          result = j - array[i];
+        }
+      }
+    }
+  }
+  return result;
 }
